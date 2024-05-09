00c3f1f775cf8f27c351f8f4f3769b2e61d97c44
Ceme4k1n <117478188+Ceme4k1n@users.noreply.github.com>
2024-05-09 13:18:57 +0300

    Session_2_AllFront

---
+++ app/src/main/java/com/example/meta/Session_2/Chose_Ptatform_Three.kt
@@ -0,0 +1,21 @@
+package com.example.meta.Session_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.meta.R
+
+class Chose_Ptatform_Three : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chose_ptatform_three)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
